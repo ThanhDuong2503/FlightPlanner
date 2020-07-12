ff025b9a102aa83be736923a432438df156f8653
ThanhDuong2503 <thanh.dn2503@gmail.com>
2020-07-12 23:45:57 +0200

    update buttons on appbar

--- frontend/src/components/MainAppBar/MainAppBar.js
+++ frontend/src/components/MainAppBar/MainAppBar.js
@@ -13,10 +13,13 @@ import { removeJWTToken } from "../../utils/jwt-utils";
 import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
 import CloudOutlinedIcon from '@material-ui/icons/CloudOutlined';
 import ExploreOutlinedIcon from '@material-ui/icons/ExploreOutlined';
+import SettingsOutlinedIcon from '@material-ui/icons/SettingsOutlined';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const useStyles = makeStyles(() => ({
   title: {
-    flexGrow: 1,
+    // flexGrow: 1,
   },
 }));
 
@@ -24,27 +27,54 @@ function MainAppBar() {
   const classes = useStyles();
   const { authStatus, userData } = useContext(UserStateContext);
   const dispatch = useContext(UserDispatchContext);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6" className={classes.title}>
-          Welcome to AeroPath {userData && userData.sub}
-        </Typography>
+
         <Button variant="contained" color="default" startIcon={<HomeOutlinedIcon />}>Home</Button>
         <Button variant="contained" color="default" startIcon={<ExploreOutlinedIcon />}>Map</Button>
         <Button variant="contained" color="default" startIcon={<CloudOutlinedIcon />}>Weather</Button>
-        {authStatus === "SUCCESS" && (
-          <Button
-              variant="contained"
-              color="default"
-              onClick={() => {
-              dispatch({ type: LOGOUT });
-              removeJWTToken();
-            }}
-          >
-            Logout
+
+        <div>
+          <Button variant="contained" color="default" startIcon={<SettingsOutlinedIcon />} aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+            Settings
           </Button>
-        )}
+          <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+          >
+            <Typography variant="h6" className={classes.title}>
+              Hello {userData && userData.sub}
+            </Typography>
+
+            <MenuItem onClick={handleClose}>Switch Mode</MenuItem>
+
+            {authStatus === "SUCCESS" && (
+                <Button
+                    variant="contained"
+                    color="default"
+                    onClick={() => {
+                      dispatch({ type: LOGOUT });
+                      removeJWTToken();
+                    }}
+                >
+                  Logout
+                </Button>
+            )}
+          </Menu>
+        </div>
       </Toolbar>
     </AppBar>
   );
