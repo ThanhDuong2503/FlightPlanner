0d44c58db5476676b659730b4ed8bfd733759998
ThanhDuong2503 <thanh.dn2503@gmail.com>
2020-07-16 09:22:11 +0200

    fixed GitHub Key

--- frontend/src/components/Map/MapContainer.js
+++ frontend/src/components/Map/MapContainer.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 
 const mapStyles = {
     width: '100%',
-    height: '100%'
+    height: '100%',
+    position: 'relative',
 };
 
 export class MapContainer extends Component {
+    state = {
+        showingInfoWindow: false,  //Hides or shows the infoWindow
+        activeMarker: {},          //Shows active marker on click
+        selectedPlace: {}          //Shows infoWindow to the selected place of a marker
+    };
+
+    // show infoWindow on click
+    onMarkerClick = (props, marker, e) =>
+        this.setState({
+            selectedPlace: props,
+            activeMarker: marker,
+            showingInfoWindow: true
+        });
+
+    // close infoWindow on click
+    onClose = props => {
+        if (this.state.showingInfoWindow) {
+            this.setState({
+                showingInfoWindow: false,
+                activeMarker: null
+            });
+        }
+    };
+
     render() {
         return (
             <Map
@@ -16,8 +41,21 @@ export class MapContainer extends Component {
                 initialCenter={{
                     lat: 52.133891,
                     lng: 7.685239
-                }}
+                }}>
+            <Marker
+                onClick={this.onMarkerClick}
+                name={"Flughafen Münster-Osnabrück"}
             />
+            <InfoWindow
+                marker={this.state.activeMarker}
+                visible={this.state.showingInfoWindow}
+                onClose={this.onClose}
+            >
+                <div>
+                    <h3>{this.state.selectedPlace.name}</h3>
+                </div>
+            </InfoWindow>
+            </Map>
         );
     }
 }
