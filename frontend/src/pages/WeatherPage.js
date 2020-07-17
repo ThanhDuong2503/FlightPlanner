de1fd249921ea7832d2cb12fbc150f31f483bca5
ThanhDuong2503 <thanh.dn2503@gmail.com>
2020-07-17 10:38:30 +0200

    test gitignore

--- frontend/src/pages/WeatherPage.js
+++ frontend/src/pages/WeatherPage.js
@@ -7,6 +7,7 @@ function WeatherPage() {
             <MainAppBar></MainAppBar>
             <h1>here comes the weather</h1>
             <h2>awesome graphics come here</h2>
+            <h3>hope i have enough time for that :D</h3>
         </div>
     )
 }
