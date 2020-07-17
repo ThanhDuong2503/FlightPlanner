e8bd2446ce7217e6a7f17a05bdaaf2cad2412afc
ThanhDuong2503 <thanh.dn2503@gmail.com>
2020-07-17 22:02:24 +0200

    add test flight route

--- frontend/src/components/Map/MapContainer.js
+++ frontend/src/components/Map/MapContainer.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Map, GoogleApiWrapper, InfoWindow, Marker} from 'google-maps-react';
+import {Map, GoogleApiWrapper, InfoWindow, Marker, Polyline} from 'google-maps-react';
 
 const mapStyles = {
     width: '100%',
@@ -28,25 +28,46 @@ export function MapContainer({google}) {
         }
     }
 
-    // const mapClick(mapProps, map, clickEvent) {
-    //     console.log(mapProps, map, clickEvent)
-    // }
+    // coords of Münster-Osnabrück Airport
+    const FMOAirport = {
+        lat: 52.133891,
+        lng: 7.685239
+    }
+
+    // action on click at some points of the map
+    const onMapClick = (mapProps, map, clickEvent) => {
+        console.log(mapProps, map, clickEvent)
+    }
+
+    // coordinates for flight route
+    const waypointCoords = [
+        {lat: 52.133891, lng: 7.685239},
+        {lat: 52.095680, lng: 7.617196},
+        {lat: 51.958669, lng: 7.622893},
+        {lat: 52.035371, lng: 7.829705},
+        {lat: 52.133891, lng: 7.685239}
+    ];
 
     return (
         <Map
             google={google}
             zoom={14}
             style={mapStyles}
-            initialCenter={{
-                lat: 52.133891,
-                lng: 7.685239
-            }}
-            // onClick={mapClick}
+            initialCenter={FMOAirport}
+            onClick={onMapClick}
         >
+            <Polyline
+                path={waypointCoords}
+                strokeColor="#3d5afe"
+                strokeOpacity={0.8}
+                strokeWeight={4}
+            />
+
             <Marker
                 onClick={onMarkerClick}
-                name={"Flughafen Münster-Osnabrück"}
+                name={"current Location"}
             />
+
             <InfoWindow
                 marker={activeMarker}
                 visible={showingInfoWindow}
